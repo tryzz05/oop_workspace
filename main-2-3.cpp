0489982e239bd76bc5320e59a971ea8bc45d0006
tryzz05 <trystanhaigh@icloud.com>
2025-03-12 16:49:49 +1030

    created main 2-3

---
+++ main-2-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+extern void two_five_nine(int array[], int n);
+
+int main() {
+    int array[] = {2, 5, 9, 2, 2, 5, 9, 6, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9};
+    int n = sizeof(array) / sizeof(array[0]);
+    
+    two_five_nine(array, n);
+    
+    return 0;
+}
