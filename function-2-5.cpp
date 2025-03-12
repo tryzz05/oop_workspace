2412d641ee133ad47182b7f20a40d7954ae6cff0
tryzz05 <trystanhaigh@icloud.com>
2025-03-12 16:43:19 +1030

    function 2-5

---
+++ function-2-5.cpp
@@ -0,0 +1,13 @@
+bool is_descending(int array[], int n) {
+    if (n <= 0) {
+        return false;
+    }
+
+    for (int i = 1; i < n; i++) {
+        if (array[i] > array[i - 1]) {
+            return false;
+        }
+    }
+    
+    return true;
+}
