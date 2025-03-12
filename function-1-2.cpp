3c24c66e118296be3cadf006d575923a7c4978a2
tryzz05 <trystanhaigh@icloud.com>
2025-03-12 15:36:22 +1030

    Function 1-2 and 1-3

---
+++ function-1-2.cpp
@@ -0,0 +1,12 @@
+double array_mean(int array[], int n) {
+    if (n < 1) {
+      return 0.0;
+    }
+  
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+      sum += array[i];
+    }
+  
+    return static_cast<double>(sum) / n;
+  }
