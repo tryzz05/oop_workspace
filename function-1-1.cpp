7b53fd56fe7f04b62b580a1f88bd87ffcddb9828
tryzz05 <trystanhaigh@icloud.com>
2025-03-12 15:22:20 +1030

    First commit for function-1-1

---
+++ function-1-1.cpp
@@ -0,0 +1,10 @@
+int array_sum(int array[], int n) {
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+      if (array[i] > 0) {
+        sum += array[i];
+      }
+    }
+  
+    return sum;
+  }
