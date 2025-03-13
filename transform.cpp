6fac09f920566c538a1a71592c0e90ded135340f
tryzz05 <trystanhaigh@icloud.com>
2025-03-13 23:13:48 +1030

    saved file?

--- transform.cpp
+++ transform.cpp
@@ -1,1 +1,22 @@
-// transform
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string convertToBinary(int n) {
+    if (n == 0) return "0";
+    string binary = "";
+    while (n > 0) {
+        binary = to_string(n % 2) + binary;
+        n /= 2;
+    }
+    return binary;
+}
+
+int main() {
+    int num;
+    cout << "Enter a decimal number: ";
+    cin >> num;
+    cout << "Binary representation: " << convertToBinary(num) << endl;
+    return 0;
+}
