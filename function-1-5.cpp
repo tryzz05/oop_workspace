2acac54da5b376472e95fd709a0adbd15c299c1b
tryzz05 <trystanhaigh@icloud.com>
2025-03-14 02:13:36 +1030

    fixed issue with i -> j

--- function-1-5.cpp
+++ function-1-5.cpp
@@ -4,13 +4,13 @@ void print_summed(int array1[3][3],int array2[3][3]) {
     int summed_array[3][3] = {0};
 
     for (int i = 0; i < 3; ++i) {
-        for (int j = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
             summed_array[i][j] = array1[i][j] + array2[i][j];
         }
     }
 
     for (int i = 0; i < 3; ++i) {
-        for (int j = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
             std::cout << summed_array[i][j] << " ";
         }
         std::cout << std::endl;
