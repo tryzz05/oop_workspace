d0ffd59ee1db740385d4635b1f7217c87a695b23
tryzz05 <trystanhaigh@icloud.com>
2025-03-19 13:39:31 +1030

    formatted main 2-4:

--- main-2-4.cpp
+++ main-2-4.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-int sum_min_max (int integers[], int length);
+int sum_min_max(int integers[], int length);
 
-int main() {
-    int matrix[] = {1,2,3,4,5,-1,6,7,8,9};
+int main()
+{
+    int matrix[] = {1, 2, 3, 4, 5, -1, 6, 7, 8, 9};
     int num_values = sizeof(matrix) / sizeof(matrix[0]);
 
     std::cout << sum_min_max(matrix, num_values) << std::endl;
