1d2473e9fd4d6aeaa0aa0ca3291cf8d97977e16c
tryzz05 <trystanhaigh@icloud.com>
2025-08-22 13:28:09 +0930

    changed greater than to less than

--- function-1-2.cpp
+++ function-1-2.cpp
@@ -1,11 +1,10 @@
 #include "Person.h"
 
-PersonList createPersonList(int n)
-{
+PersonList createPersonList(int n) {
     PersonList pList;
-    pList.people = new Person[n];
-    pList.numPeople = n;
-    for (int i = 0; i > n; ++i) {
+        pList.numPeople = n;
+        pList.people = new Person[n];
+    for (int i = 0; i < n; ++i) {
         pList.people[i].name = "Jane Doe";
         pList.people[i].age = 1;
     }
