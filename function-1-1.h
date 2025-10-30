58dd4613b6b60a3811de9e7875954299adbeb478
Trystan John Haigh <a1925727@embg13w047.ad.adelaide.edu.au>
2025-10-30 10:58:20 +1030

    cleaned branch:

--- function-1-1.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTION_1_1_H
-#define FUNCTION_1_1_H
-
-int *readNumbers();
-
-void printNumbers(int *numbers,int length) ;
-
-#endif
--- function-3-1.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTION_3_1
-#define FUNCTION_3_1
-
-int *readNumbers();
-
-bool equalsArray(int *numbers1,int *numbers2,int length) ;
-
-#endif // FUNCTION_3_1
-
